9a1db3211236e92f5c8e3d81ef244612dbe8fe69
Romain Campech <romain.campech@city.ac.uk>
2017-08-30 20:36:45 +0100

    Setup main react component Chatroom

---
+++ app/javascript/chatroom/components/chatroom.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Chatroom extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
+}
+
+export default Chatroom;
---
+++ app/javascript/chatroom/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Chatroom from './components/chatroom';
+
+const chatroom = document.querySelector('.container');
+
+ReactDOM.render(<Chatroom />, chatroom)
